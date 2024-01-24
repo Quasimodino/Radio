48db5a5c14f0e071d31b0f26b9449b6e940a8a99
atadd <atadd@DESKTOP-PVF3S8C>
2024-01-24 17:52:41 +0100

    Aggiungere i file di progetto.

---
+++ Radio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            Console.WriteLine(" ");
+            radio.volume=float.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", radio.aumentavolume());
+            Console.WriteLine("{0}", radio.diminuiscivolume());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Radio/Radio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    internal class Radio
+    {
+        public float volume { get; set; }
+        public float aumentavolume()
+        {
+            float vol= volume + volume / 10;
+            return vol;
+        }
+        public float diminuiscivolume()
+        {
+            float vol = volume - volume / 10;
+            return vol;
+        }
+    }
+}
